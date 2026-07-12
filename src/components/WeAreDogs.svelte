980dcd4039f4aabd4b15a47442c48441bed716fc
CaptainBrando! <82909107+cptnbrando@users.noreply.github.com>
2026-07-12 18:07:43 -0500

    style: set title font-family to Fira Code

--- src/components/WeAreDogs.svelte
+++ src/components/WeAreDogs.svelte
@@ -1609,9 +1609,9 @@
     flex-wrap: nowrap;
     margin: 0;
     padding: 0;
-    font-family: "Inconsolata", ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
+    font-family: "Fira Code", ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
     font-size: clamp(5rem, 18vmin, 15rem);
-    font-weight: 900;
+    font-weight: 700;
     line-height: 1.05;
     gap: 0.18em;
     text-transform: uppercase;
