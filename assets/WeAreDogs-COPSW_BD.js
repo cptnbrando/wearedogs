ba007e4e57a23ac8ffb340522656c11bb06151be
cptnbrando <82909107+cptnbrando@users.noreply.github.com>
2026-07-13 04:19:47 +0000

    Deploying to gh-pages from @ cptnbrando/wearedogs@6121b26d857dbab1bcff81774b29f96fb6a3b9f9 🚀

---
+++ assets/WeAreDogs-COPSW_BD.js
@@ -0,0 +1,1 @@
+import{t as e}from"./TitlePage-DKWBZU13.js";export{e as default};
---
+++ assets/WeAreDogs-legacy-eXIhGn8X.js
@@ -0,0 +1,1 @@
+System.register(["./TitlePage-legacy-Al1gkE66.js"],function(e,t){var n;return{setters:[function(e){n=e.t}],execute:function(){e("default",n)}}});
